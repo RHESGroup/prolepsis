e293d0e62e9311621962e16888d985889f15b585
MrAle98 <ale18109800@gmail.com>
2022-09-07 11:21:57 +0200

    modified sign_generation_grouping

--- risc-v-cfi-tool/sign_generation_grouping.py
+++ risc-v-cfi-tool/sign_generation_grouping.py
@@ -15,35 +15,19 @@ def brecking(x):
 
 def rebrecking(x):
   powers = brecking(x)
-  if (x > 8):
+  if (x >= 8):
     while len(powers) < 8:
       if powers[-1] > 1 :
         powers.append((powers[-1])/2)
         powers.append((powers[-2])/2)
         powers.sort()
         powers.pop()
+  else:
+    for i in range(len(powers),8):
+      powers.append(0)
   return powers
 
-#assign random member for each group
-def mk_gr (int_primes, x, flag, numNeq, bits):
-  m = []
-  notm = []
-  notVal = 0
-  for i in range (0, len(x)):
-    temp = format(i, "b").zfill(3)
-    res = [int(x) for x in str(temp)]
-    res = res[::-1]
-    m.append(int_primes[0:x[i]])
-    notm.append(int_primes[x[i]:bits])
-    temp = format(random.randint(0, 2**(len(m[i])-5)), "b").zfill(len(m[i])-5)
-    res2 = [int(x) for x in str(temp)]
-    res= res+ res2
-    for j in reversed(range(len(res))):
-      if res[j] == 1:
-        m[i][j] = "~"+ str(m[i][j])
-        m[i].pop(j)
-  return m, notm
-  
+
 # generate configurable memory data
 def bin_config_mem(a, nota):  
   myarr=[""]*10 
@@ -53,7 +37,9 @@ def bin_config_mem(a, nota):
     for i in range(20):
       n = m//16
       m += 1
-      if i in a[j]:
+      if (not a[j] and not nota[j]):
+        myarr[n] = "11"+myarr[n]
+      elif i in a[j]:
         myarr[n] = "01"+myarr[n]
       elif i in nota[j]:
         myarr[n] = "00"+myarr[n]
@@ -68,11 +54,43 @@ def gen_primes(x):
     for i in range (0, len(x)):
         res += 2**x[i]
     return res
-	
+
+#Call different groups
+def call_groups(primes,x,y,bits):
+  prime1 =[]
+  for i in range(0, len(x)):
+    prime1.append(rec_group(x[i],y[i], bits-1, [])) 
+  flat_list = [item for sublist in prime1 for item in sublist]
+  return flat_list
+#assign random member for each group
+def mk_gr (int_primes, x, flag, numNeq, bits):
+  m = []
+  notm = []
+  notVal = 0
+  zeros =list(range(0, bits))
+  for i in range (0, len(x)):
+    temp = format(i, "b").zfill(3)
+    res = [int(x) for x in str(temp)]
+    res = res[::-1]
+    if (x[i] > 0):
+      m.append(int_primes[0:x[i]])
+      notm.append(int_primes[x[i]:bits])
+      temp = format(random.randint(0, 2**(len(m[i])-5)), "b").zfill(len(m[i])-5)
+      res2 = [int(x) for x in str(temp)]
+      res= res+ res2
+      for j in reversed(range(len(res))):
+        if res[j] == 1:
+          m[i][j] = "~"+ str(m[i][j])
+          m[i].pop(j)
+    else:
+      m.append([])
+      notm.append([])
+  return m, notm
+  
 #Generate the group members 
 def rec_group(ones, challenge, numberOfBits, generatedSig):
   newOnes = ones[:]
-  if numberOfBits >= 0:
+  if (numberOfBits >= 0):
     if not numberOfBits in challenge:
       rec_group(newOnes, challenge, numberOfBits-1, generatedSig)
     else:
@@ -82,30 +100,23 @@ def rec_group(ones, challenge, numberOfBits, generatedSig):
         else :
           newOnes.append(numberOfBits)
           rec_group(newOnes, challenge, numberOfBits-1, generatedSig)
-  else:
+  elif challenge or ones:
     i = gen_primes(newOnes)
     if i not in generatedSig:
       generatedSig.append(i)
   return generatedSig
-
-#Call different groups
-def call_groups(primes,x,y,bits):
-  prime1 =[]
-  for i in range(0, len(x)):
-    prime1.append(rec_group(x[i],y[i], bits-1, []))
-    
-  flat_list = [item for sublist in prime1 for item in sublist]
-  return flat_list
-
+  
 #The main function to call and generate signatures
 def gen_number(num , bits):
   x = rebrecking(num)
+  zeros = x.count(0)
   x = [int(a) for a in x]
-  y = [int(bits-math.log2(a)) for a in x]
+  y = [int(bits-math.log2(a)) for a in x if (a > 0)]
+  y.extend([0] * zeros)
   primes = list(range((bits)))
   m, n = mk_gr (primes, y, 0, 0, bits)
   l = call_groups(primes,m,n,bits)
   return l, m, n
 
-gen_number(14,20)
+gen_number(5,20)
 
